388239b43773525f8d81781e067e4727c83ddd8b
mansour90 <mansour_alaa82@yahoo.ro>
2023-06-03 00:19:23 +0300

    Counter fixed

--- script/app.js
+++ script/app.js
@@ -1,27 +1,28 @@
-let counterBox = document.getElementById('counter-number'); 
-let tabBtn = document.getElementById('tap'); 
-let resetBtn = document.getElementById('reset'); 
-let images = document.getElementById('images'); 
-console.log(counterBox.textContent)
+let counterBox = document.getElementById('counter-number');
+let tabBtn = document.getElementById('tap');
+let resetBtn = document.getElementById('reset');
+let images = document.getElementById('images');
+console.log(counterBox.textContent);
 
-let count = 0
+let count = 0;
 function add() {
-  count = count +1
-  counterBox.innerText = count
+  count = count + 1;
+  counterBox.innerText = count;
 
   if (counterBox.textContent >= 100) {
     images.src = 'images/2.svg';
+    counterBox.innerText = 0;
   }
   if (counterBox.textContent >= 200) {
     images.src = 'images/3.svg';
+    counterBox.innerText = 0;
   }
   if (counterBox.textContent >= 300) {
     images.src = 'images/4.svg';
+    counterBox.innerText = 0;
   }
 }
 
-resetBtn.addEventListener('click',function() {
-  counterBox.innerText = 0
-})
-
-
+resetBtn.addEventListener('click', function () {
+  counterBox.innerText = 0;
+});
